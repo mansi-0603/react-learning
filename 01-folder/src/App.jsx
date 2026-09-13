82ec78feb404f878f5085131deac3ad6d35779ac
Mansi Srivastava <srimansi2006@gmail.com>
2026-09-13 13:21:18 +0530

    Initial React setup

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h4>hii! this is mansi.</h4>
+      <p>I am trying to learn React + Vite for first time and to be honest.
+      React for me is actually quite confusing right now. All these vite configurations are very complicating for me.
+      But I will try hard to learn all these complications.
+      </p>
+
+    </div>
+  )
+}
+
+export default App
