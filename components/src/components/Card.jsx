c56a9553fa7906da4e0f0fb0a73eeee7dcc44812
Mansi Srivastava <srimansi2006@gmail.com>
2026-09-15 11:16:11 +0530

    This is a component practice file

---
+++ components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+      <h3>hii! this is about components.</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo necessitatibus ipsum doloribus fuga magni deserunt reiciendis eligendi debitis autem laboriosam!</p>
+     </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ components/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        <h5>hii this is a navbar section which will contain an image of xyz person in the future. </h5>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
